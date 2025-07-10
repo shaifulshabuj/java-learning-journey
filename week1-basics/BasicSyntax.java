d0b0894c78b6c0afa913da9ef484b41d5bd6aaab
shaifulshabuj <shaifulshabuj@gmail.com>
2025-07-10 20:54:04 +0900

    Day 1: First Java program - Hello World and BasicSyntex

---
+++ week1-basics/BasicSyntax.java
@@ -0,0 +1,34 @@
+//This is my first Java program
+public class BasicSyntax { // Class declaration
+    public static void main(String[] args) { // Main method
+        System.out.println("Hello, World!"); // Output statement
+
+        // 1. Variables and Data Types
+        int age = 25;
+        double salary = 50000.50;
+        String name = "Java Developer";
+        boolean isLearning = true;
+
+        // 2. Output different data types
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: $" + salary);
+        System.out.println("Is Learning: " + isLearning);
+
+        // 3. Simple calculation
+        int firstNumber = 10;
+        int secondNumber = 20;
+        int sum = firstNumber + secondNumber;
+        System.out.println("Sum of " + firstNumber + " and " + secondNumber + " is: " + sum);
+
+        // 4. String operations
+        String greeting = "Hello";
+        String target = "Java";
+        String message = greeting + " " + target + "!";
+
+        System.out.println(message);
+        System.out.println("Message length: " + message.length());
+        System.out.println("Uppercase: " + message.toUpperCase());
+
+    }
+}
---
+++ week1-basics/HelloWorld.java
@@ -0,0 +1,6 @@
+//This is my first Java program
+public class HelloWorld { // Class declaration
+    public static void main(String[] args) { // Main method
+        System.out.println("Hello, World!"); // Output statement
+    }
+}
